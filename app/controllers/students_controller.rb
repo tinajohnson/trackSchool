f8363f29b1d93173cc4efe88ed4e1849892634fc
Aravind K N <aravikn@gmail.com>
2016-07-27 00:42:28 +0530

    Created method 'list_students'

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -50,6 +50,29 @@ class StudentsController < InheritedResources::Base
       end
     end
   end
+  
+  def list_students
+    std = params["standard"]
+    sec = params["section"]
+
+    @mapping_id = 0
+
+    std_id = Standard.find_by_standard_name(std)
+    sec_id = Section.find_by_section_name(sec)
+
+
+    c_map = ClassMapping.all
+    c_map.each do |c_map|
+      if (c_map.standard_id == std_id.id && c_map.section_id == sec_id.id)
+        @mapping_id = c_map.id
+        end
+      end
+
+     if(@mapping_id == 0)
+       render template: "students/error"
+     end
+
+    end
 
   def show_selected_id
 
