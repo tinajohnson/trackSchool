2bc7ff73c233c950dba1b2c7df2f36d9b66bb870
Aravind K N <aravikn@gmail.com>
2016-07-27 01:39:50 +0530

    To remove unwanted filters and fields

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,23 @@
 ActiveAdmin.register User  do
 
+ remove_filter :encrypted_password,:reset_password_token, :reset_password_sent_at,
+    :remember_created_at,
+    :sign_in_count,
+    :current_sign_in_at,
+    :last_sign_in_at,
+    :current_sign_in_ip,
+    :last_sign_in_ip,
+    :created_at,
+    :updated_at
+
+    permit_params :email , :role
+      index do
+         column :id
+         column :email
+         column :role
+         actions
+      end
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
