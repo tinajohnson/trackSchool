e8595661c8e8587f399173c92cf98d98c75bd7d0
Aravind K N <aravikn@gmail.com>
2016-07-28 23:34:01 +0530

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,52 @@
-class UsersController < InheritedResources::Base
+class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def teacher
-    render 'teachers/index'
+
   end
+
+  def teacher_delete
+      id = params["id"]
+      t = Allotment.find(id)
+      t.destroy
+  end
+
+  def teacher_allot_classes
+    @id = params["id"]
+  end
+
+  def class_alloted
+     user_id = params["id"]
+     std = params["standard"]
+     sec = params["section"]
+
+     mapping_id = 0
+
+     std_id = Standard.find_by_standard_name(std)
+     sec_id = Section.find_by_section_name(sec)
+
+
+     c_map = ClassMapping.all
+     c_map.each do |c_map|
+       if (c_map.standard_id == std_id.id && c_map.section_id == sec_id.id)
+         mapping_id = c_map.id
+       end
+     end
+
+    t = ClassMapping.find(mapping_id)
+    school_id = t.school_id
+
+    t = Allotment.new
+    t.user_id= user_id.to_i
+    t.school_id = school_id
+    t.class_mappings_id = mapping_id
+    t.role = "Teacher"
+    t.save
+
+     end
+
+    def render_404
+      render 'users/error'
+    end
 
 end
