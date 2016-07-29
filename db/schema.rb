10a342193976720bc4658e49f648f25a8795ba8b
Tina Johnson <tinajohnson.1234@gmail.com>
2016-07-29 21:35:07 +0530

    Add name field to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160726112315) do
+ActiveRecord::Schema.define(version: 20160729160131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +81,6 @@ ActiveRecord::Schema.define(version: 20160726112315) do
   end
 
   create_table "students", force: :cascade do |t|
-
     t.string   "name"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
@@ -104,6 +102,7 @@ ActiveRecord::Schema.define(version: 20160726112315) do
     t.datetime "updated_at",                          null: false
     t.string   "role"
     t.integer  "school_id"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
