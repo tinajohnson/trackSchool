bd5988f4e67184df85d574fc88fe95def7c003f1
Aravind K N <aravikn@gmail.com>
2016-07-31 22:16:40 +0530

    Add files via upload

---
+++ app/assets/javascripts/components/date.jsx
@@ -0,0 +1,18 @@
+var Date_Picker = React.createClass({
+    getInitialState: function() {
+        return {};
+    },
+    handleChange: function(e) {
+        console.log(e.target.value);
+        this.setState({
+            value: e.target.value
+        });
+    },
+    render: function() {
+        return (
+            <div>
+                <input type="date" onChange={this.handleChange}  name="date" id="date" content="date" />
+            </div>
+        );
+    }
+});
